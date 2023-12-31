25b14febbc8fb3dc1301756668bfb62f0b9e4fe7
Maksimka66 <maxfese@gmail.com>
2023-12-31 05:57:58 +0200

    hw is done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,6 +1,4 @@
 const formItem = document.querySelector('.feedback-form');
-const emailInput = document.querySelector('[name="email"]');
-const messageArea = document.querySelector('[name="message"]');
 
 const dataItems = {
   email: '',
@@ -8,29 +6,34 @@ const dataItems = {
 };
 
 formItem.addEventListener('input', event => {
-  event.preventDefault();
-  dataItems.email = emailInput.value.trim();
-  dataItems.message = messageArea.value.trim();
+  if (event.target.name === 'email') {
+    dataItems.email = event.target.value.trim();
+  } else if (event.target.name === 'message') {
+    dataItems.message = event.target.value.trim();
+  }
   localStorage.setItem('feedback-form-state', JSON.stringify(dataItems));
 });
 
-const filledForm = localStorage.getItem('feedback-form-state');
-
-window.addEventListener('beforeunload', event => {
-  event.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
+  const filledForm = localStorage.getItem('feedback-form-state');
   if (filledForm !== null) {
     const parsedForm = JSON.parse(filledForm);
-    emailInput.value = parsedForm.email;
-    messageArea.value = parsedForm.message;
+    formItem.elements.email.value = parsedForm.email;
+    formItem.elements.message.value = parsedForm.message;
   }
 });
 
 formItem.addEventListener('submit', event => {
   event.preventDefault();
-  if (emailInput.value === '' || messageArea.value === '') {
+  if (
+    formItem.elements.email.value === '' ||
+    formItem.elements.message.value === ''
+  ) {
     return;
   }
+  dataItems.email = formItem.elements.email.value.trim();
+  dataItems.message = formItem.elements.message.value.trim();
   console.log(dataItems);
-  localStorage.clear();
-  formItem.reset();
+  localStorage.removeItem('feedback-form-state');
+  event.currentTarget.reset();
 });
